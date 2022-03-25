53ccb477e6b6d36d4e593ab42923998a84aded17
Anish <anishrockagarwal1024@gmail.com>
2022-03-25 13:05:18 +0530

    MEtamask implementation checking

--- medkept-react/src/App.js
+++ medkept-react/src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 // import Userdashboard from './components/userDashboard/userdashboard';
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
-import DoctorDashboard from './components/doctordashboard/doctor'
+// import DoctorDashboard from './components/doctordashboard/doctor'
+import Metamask from './metamask/metamask'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <DoctorDashboard/>
+      {/* <DoctorDashboard/> */}
+      <Metamask/>
     </div>
   );
 }
