eca002704671d8d269d5c4f11dfa59763b78d2a5
riyaroy2086 <89868832+riyaroy2086@users.noreply.github.com>
2022-03-25 11:44:21 +0530

    landing page done

--- medkept-react/src/App.js
+++ medkept-react/src/App.js
@@ -1,57 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Home from "./components/pages/Home";
+import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Switch, Route } from "react-router-dom";
+import Registration from "./components/pages/Registration";
+import Reports from "./components/pages/Reports";
+import Prescription from "./components/pages/Prescription";
+import SignUp from "./components/pages/SignUp";
+import HeroSection from "./components/HeroSection";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <HeroSection/>
+        <Footer/>
+        <Routes>
+          <Route path="/" exact component={Home} />
+          <Route path="/registration" component={Registration} />
+          <Route path="/reports" component={Reports} />
+          <Route path="/prescription" component={Prescription} />
+          <Route path="/sign-up" component={SignUp} />
+          </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ medkept-react/src/components/Navbar.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect  } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome ,faNotesMedical ,faStethoscope ,faAddressCard } from "@fortawesome/free-solid-svg-icons";
+
+import "./Navbar.css";
+import { Button } from "./Button";
+
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+
+  return (
+    <>
+      <nav className="navbar">
+        <div className="navbar_container">
+          <img src="assets/pic.jpg" className="navbar_logo" />
+          <div className="name"><span>M</span>ed <span>K</span>ept <span>.</span> </div>
+
+          <div className="menu" onClick={handleClick}>
+            {/* <i className={click ? "fas fa-times" : "fas fa-bars"} /> */}
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <ul className="nav_item">
+                <Link to="/" className="nav_links" onClick={closeMobileMenu}>
+                  Home <FontAwesomeIcon className="fa_icon" icon={faHome}  />
+                </Link>
+              </ul>
+
+              <ul className="nav_item">
+                <Link
+                  to="/registration"
+                  className="nav_links"
+                  onClick={closeMobileMenu}
+                >
+                  Registration <FontAwesomeIcon className="fa_icon" icon ={faAddressCard}  />
+                  {/* Manager */}
+                </Link>
+              </ul>
+
+              <ul className="nav_item">
+                <Link
+                  to="/reports"
+                  className="nav_links"
+                  onClick={closeMobileMenu}
+                >
+                  Reports <FontAwesomeIcon className="fa_icon" icon={faStethoscope} />
+                  {/* Patients */}
+                </Link>
+              </ul>
+
+              <ul className="nav_item">
+                <Link
+                  to="/prescription"
+                  className="nav_links"
+                  onClick={closeMobileMenu}
+                >
+                  Prescription <FontAwesomeIcon className="fa_icon" icon={faNotesMedical}/>
+                  {/* doctor */}
+                </Link>
+              </ul>
+            </ul>
+            {/* {button && <Button buttonStyle="btn--outline" >SIGN UP</Button>} */}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
